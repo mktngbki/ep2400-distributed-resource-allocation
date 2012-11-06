12e8ab73ebf082a8fb4a301bedd18dbfa78802e6
sercuc@gmail.com <sercuc@gmail.com@960aee3d-9021-a212-2a8e-b595aefb4307>
2012-11-06 18:39:38 +0000

    updated proposal

--- EP2400-Distributed_Resource_Allocation/src/peersim/EP2400/resourcealloc/tasks/util/Proposal.java
+++ EP2400-Distributed_Resource_Allocation/src/peersim/EP2400/resourcealloc/tasks/util/Proposal.java
@@ -1,16 +1,16 @@
 package peersim.EP2400.resourcealloc.tasks.util;
 
-import java.util.List;
+import peersim.EP2400.resourcealloc.base.ApplicationsList;
 
 public class Proposal {
 	public enum ProposalType {
-		PUSH, PULL, NO_ACTION;
+		PUSH, PULL, BALANCE, NO_ACTION;
 	}
 	
-	private ProposalType			pType;
-	private List<ApplicationInfo>	appList;
+	private ProposalType		pType;
+	private ApplicationsList	appList;
 	
-	public Proposal(ProposalType pType, List<ApplicationInfo> appList) {
+	public Proposal(ProposalType pType, ApplicationsList appList) {
 		this.pType = pType;
 		this.appList = appList;
 	}
@@ -19,43 +19,7 @@ public class Proposal {
 		return pType;
 	}
 	
-	public List<ApplicationInfo> getApplicationsList() {
+	public ApplicationsList getApplicationsList() {
 		return appList;
 	}
-	
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((appList == null) ? 0 : appList.hashCode());
-		result = prime * result + ((pType == null) ? 0 : pType.hashCode());
-		return result;
-	}
-	
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (obj == null) {
-			return false;
-		}
-		if (getClass() != obj.getClass()) {
-			return false;
-		}
-		Proposal other = (Proposal) obj;
-		if (appList == null) {
-			if (other.appList != null) {
-				return false;
-			}
-		} else if (!appList.equals(other.appList)) {
-			return false;
-		}
-		if (pType != other.pType) {
-			return false;
-		}
-		return true;
-	}
-	
-	
 }
