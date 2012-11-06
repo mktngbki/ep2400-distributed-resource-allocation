cfc44175e614a5e4619b387b54fcf10f7eb2178f
sercuc@gmail.com <sercuc@gmail.com@960aee3d-9021-a212-2a8e-b595aefb4307>
2012-11-06 20:01:20 +0000

    LoadBalanceStrategy and EnergyEfficientStrategy updated

--- trunk/EP2400-Distributed_Resource_Allocation/src/peersim/EP2400/resourcealloc/tasks/placementStartegy/EnergyEfficientStrategy.java
+++ trunk/EP2400-Distributed_Resource_Allocation/src/peersim/EP2400/resourcealloc/tasks/placementStartegy/EnergyEfficientStrategy.java
@@ -1,13 +1,11 @@
 package peersim.EP2400.resourcealloc.tasks.placementStartegy;
 
-import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
-import java.util.List;
-import java.util.Set;
+import java.util.HashSet;
 
 import peersim.EP2400.resourcealloc.base.ApplicationsList;
 import peersim.EP2400.resourcealloc.tasks.util.AppCPUComparator;
-import peersim.EP2400.resourcealloc.tasks.util.ApplicationInfo;
 import peersim.EP2400.resourcealloc.tasks.util.Proposal;
 import peersim.EP2400.resourcealloc.tasks.util.Proposal.ProposalType;
 
@@ -15,25 +13,26 @@ public class EnergyEfficientStrategy extends PlacementStrategy {
 	private static final double	CPU_USAGE_THRESHOLD	= 70;
 	
 	@Override
-	public synchronized Proposal getProposal(final ApplicationsList ownAppList, final ApplicationsList partnerAppList, final Set<Integer> partnerReceivedApps) {
-		double ownCPUUsage = getCPUUsage(ownAppList);
-		double partnerCPUUsage = getCPUUsage(partnerAppList);
-		ProposalType pType;
-		List<ApplicationInfo> propAppList;
+	public synchronized Proposal getProposal(final ApplicationsList ownAppList, final ApplicationsList partnerAppList,
+		final Collection<Integer> ownReceivedApps, final Collection<Integer> partnerReceivedApps, final Collection<Integer> ownPromisedApps) {
+		double ownCPUUsage = ownAppList.totalCPUDemand();
+		double partnerCPUUsage = partnerAppList.totalCPUDemand();
+		ProposalType pType = null;
+		ApplicationsList propAppList = null;
 		
 		double maxCPUUsage = Math.max(ownCPUUsage, partnerCPUUsage);
 		
 		if (maxCPUUsage == partnerCPUUsage && partnerCPUUsage < CPU_USAGE_THRESHOLD) {
 			pType = ProposalType.PUSH;
 			Collections.sort(ownAppList, new AppCPUComparator());
-			propAppList = getAppListToPropose(CPU_USAGE_THRESHOLD - partnerCPUUsage, ownAppList, leasedAppList);
+			propAppList = getAppListToPropose(CPU_USAGE_THRESHOLD - partnerCPUUsage, ownAppList, ownReceivedApps, ownPromisedApps);
 		} else if (maxCPUUsage == ownCPUUsage && ownCPUUsage < CPU_USAGE_THRESHOLD) {
 			pType = ProposalType.PULL;
 			Collections.sort(partnerAppList, new AppCPUComparator());
-			propAppList = getAppListToPropose(CPU_USAGE_THRESHOLD - ownCPUUsage, partnerAppList, new ArrayList<ApplicationInfo>());
+			propAppList = getAppListToPropose(CPU_USAGE_THRESHOLD - ownCPUUsage, partnerAppList, partnerReceivedApps, new HashSet<Integer>());
 		} else {
 			pType = ProposalType.NO_ACTION;
-			propAppList = new ArrayList<ApplicationInfo>();
+			propAppList = new ApplicationsList();
 		}
 		
 		return new Proposal(pType, propAppList);
--- trunk/EP2400-Distributed_Resource_Allocation/src/peersim/EP2400/resourcealloc/tasks/placementStartegy/LoadBalanceStrategy.java
+++ trunk/EP2400-Distributed_Resource_Allocation/src/peersim/EP2400/resourcealloc/tasks/placementStartegy/LoadBalanceStrategy.java
@@ -1,33 +1,32 @@
 package peersim.EP2400.resourcealloc.tasks.placementStartegy;
 
-import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
-import java.util.List;
-import java.util.Set;
+import java.util.HashSet;
 
 import peersim.EP2400.resourcealloc.base.ApplicationsList;
 import peersim.EP2400.resourcealloc.tasks.util.AppCPUComparator;
-import peersim.EP2400.resourcealloc.tasks.util.ApplicationInfo;
 import peersim.EP2400.resourcealloc.tasks.util.Proposal;
 import peersim.EP2400.resourcealloc.tasks.util.Proposal.ProposalType;
 
 public class LoadBalanceStrategy extends PlacementStrategy {
 	
 	@Override
-	public synchronized Proposal getProposal(final ApplicationsList ownAppList, final ApplicationsList partnerAppList, final Set<Integer> partnerReceivedApps) {
-		double ownCPUUsage = getCPUUsage(ownAppList);
-		double partnerCPUUsage = getCPUUsage(partnerAppList);
-		ProposalType pType;
-		List<ApplicationInfo> propAppList;
+	public synchronized Proposal getProposal(final ApplicationsList ownAppList, final ApplicationsList partnerAppList,
+		final Collection<Integer> ownReceivedApps, final Collection<Integer> partnerReceivedApps, final Collection<Integer> ownPromisedApps) {
+		double ownCPUUsage = ownAppList.totalCPUDemand();
+		double partnerCPUUsage = partnerAppList.totalCPUDemand();
+		ProposalType pType = null;
+		ApplicationsList propAppList = null;
 		
 		if (ownCPUUsage > partnerCPUUsage) {
 			pType = ProposalType.PUSH;
 			Collections.sort(ownAppList, new AppCPUComparator());
-			propAppList = getAppListToPropose((ownCPUUsage - partnerCPUUsage) / 2, ownAppList, leasedAppList);
+			propAppList = getAppListToPropose((ownCPUUsage - partnerCPUUsage) / 2, ownAppList, ownReceivedApps, ownPromisedApps);
 		} else {
 			pType = ProposalType.PULL;
 			Collections.sort(partnerAppList, new AppCPUComparator());
-			propAppList = getAppListToPropose((partnerCPUUsage - ownCPUUsage) / 2, partnerAppList, new ArrayList<ApplicationInfo>());
+			propAppList = getAppListToPropose((partnerCPUUsage - ownCPUUsage) / 2, partnerAppList, partnerReceivedApps, new HashSet<Integer>());
 		}
 		
 		return new Proposal(pType, propAppList);
