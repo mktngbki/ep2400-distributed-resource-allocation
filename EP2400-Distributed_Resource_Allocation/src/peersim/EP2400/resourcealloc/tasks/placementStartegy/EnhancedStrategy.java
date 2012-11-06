5fc7d8e64c4195c54ea93c46f15c86fbc2735102
sercuc@gmail.com <sercuc@gmail.com@960aee3d-9021-a212-2a8e-b595aefb4307>
2012-11-06 19:14:41 +0000

    BROKEN

--- EP2400-Distributed_Resource_Allocation/src/peersim/EP2400/resourcealloc/tasks/placementStartegy/EnhancedStrategy.java
+++ EP2400-Distributed_Resource_Allocation/src/peersim/EP2400/resourcealloc/tasks/placementStartegy/EnhancedStrategy.java
@@ -1,11 +1,12 @@
 package peersim.EP2400.resourcealloc.tasks.placementStartegy;
 
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
+import peersim.EP2400.resourcealloc.base.Application;
+import peersim.EP2400.resourcealloc.base.ApplicationsList;
 import peersim.EP2400.resourcealloc.tasks.util.AppCPUComparator;
-import peersim.EP2400.resourcealloc.tasks.util.ApplicationInfo;
 import peersim.EP2400.resourcealloc.tasks.util.Proposal;
 import peersim.EP2400.resourcealloc.tasks.util.Proposal.ProposalType;
 
@@ -13,48 +14,97 @@ public class EnhancedStrategy extends PlacementStrategy {
 	private static final double	CPU_USAGE_THRESHOLD	= 70;
 	
 	@Override
-	public synchronized Proposal getProposal(final List<ApplicationInfo> ownAppList, final List<ApplicationInfo> partnerAppList,
-		final List<ApplicationInfo> leasedAppList) {
-		double ownCPUUsage = getCPUUsage(ownAppList);
-		double partnerCPUUsage = getCPUUsage(partnerAppList);
-		ProposalType pType;
-		List<ApplicationInfo> propAppList;
+	public synchronized Proposal getProposal(final ApplicationsList ownAppList, final ApplicationsList partnerAppList, final Set<Integer> ownReceivedApps,
+		final Set<Integer> partnerReceivedApps, final Set<Integer> ownPromisedApps) {
+		double ownCPUUsage = ownAppList.totalCPUDemand();
+		double partnerCPUUsage = partnerAppList.totalCPUDemand();
+		ProposalType pType = null;
+		ApplicationsList propAppList = null;
 		
-		// Apply Energy Efficient Strategy if both the nodes are not overloaded
-		if (ownCPUUsage < CPU_USAGE_THRESHOLD && partnerCPUUsage < CPU_USAGE_THRESHOLD) {
-			if (partnerCPUUsage > ownCPUUsage) {
+		//Apply Energy Efficient Strategy if the sum of the CPU Usage of both nodes is below threshold * 2
+		if (ownCPUUsage + partnerCPUUsage <= CPU_USAGE_THRESHOLD * 2) {
+			// Give Apps to the node that has more CPU usage if both the nodes are under the threshold
+			if (ownCPUUsage < CPU_USAGE_THRESHOLD && partnerCPUUsage < CPU_USAGE_THRESHOLD) {
+				if (partnerCPUUsage > ownCPUUsage) {
+					pType = ProposalType.PUSH;
+					Collections.sort(ownAppList, new AppCPUComparator());
+					propAppList = getAppListToPropose(CPU_USAGE_THRESHOLD - partnerCPUUsage, ownAppList, ownReceivedApps, ownPromisedApps);
+				} else {
+					pType = ProposalType.PULL;
+					Collections.sort(partnerAppList, new AppCPUComparator());
+					propAppList = getAppListToPropose(CPU_USAGE_THRESHOLD - ownCPUUsage, partnerAppList, partnerReceivedApps, new HashSet<Integer>());
+				}
+			}
+			// Apply Load Balance Strategy if one of the nodes is overloaded (Maximize energy efficiency)
+			else if (ownCPUUsage > CPU_USAGE_THRESHOLD && partnerCPUUsage < CPU_USAGE_THRESHOLD) {
 				pType = ProposalType.PUSH;
 				Collections.sort(ownAppList, new AppCPUComparator());
-				propAppList = getAppListToPropose(CPU_USAGE_THRESHOLD - partnerCPUUsage, ownAppList, leasedAppList);
-			} else {
+				propAppList = getAppListToPropose(ownCPUUsage - CPU_USAGE_THRESHOLD, ownAppList, ownReceivedApps, ownPromisedApps, true);
+			} else if (partnerCPUUsage > CPU_USAGE_THRESHOLD && ownCPUUsage < CPU_USAGE_THRESHOLD) {
 				pType = ProposalType.PULL;
 				Collections.sort(partnerAppList, new AppCPUComparator());
-				propAppList = getAppListToPropose(CPU_USAGE_THRESHOLD - ownCPUUsage, partnerAppList, new ArrayList<ApplicationInfo>());
+				propAppList = getAppListToPropose(partnerCPUUsage - CPU_USAGE_THRESHOLD, partnerAppList, partnerReceivedApps, new HashSet<Integer>(), true);
 			}
 		}
-		// Apply Load Balance Strategy if one of the nodes is overloaded (Maximize energy efficiency)
-		else if (ownCPUUsage > CPU_USAGE_THRESHOLD && partnerCPUUsage < CPU_USAGE_THRESHOLD) {
-			pType = ProposalType.PUSH;
-			Collections.sort(ownAppList, new AppCPUComparator());
-			propAppList = getAppListToPropose(ownCPUUsage - CPU_USAGE_THRESHOLD, ownAppList, leasedAppList, true);
-		} else if (partnerCPUUsage > CPU_USAGE_THRESHOLD && ownCPUUsage < CPU_USAGE_THRESHOLD) {
-			pType = ProposalType.PULL;
-			Collections.sort(partnerAppList, new AppCPUComparator());
-			propAppList = getAppListToPropose(partnerCPUUsage - CPU_USAGE_THRESHOLD, partnerAppList, new ArrayList<ApplicationInfo>(), true);
-		}
-		// Apply pure Load Balance Strategy if both the nodes are overloaded 
+		// Apply pure Load Balance Strategy if both the nodes are overloaded
 		else {
 			if (ownCPUUsage > partnerCPUUsage) {
-				pType = ProposalType.PUSH;
+				pType = ProposalType.OVERLOADED_PUSH;
 				Collections.sort(ownAppList, new AppCPUComparator());
-				propAppList = getAppListToPropose((ownCPUUsage - partnerCPUUsage) / 2, ownAppList, leasedAppList);
+				propAppList = getAppListToPropose((ownCPUUsage - partnerCPUUsage) / 2, ownAppList, ownReceivedApps, ownPromisedApps);
 			} else {
 				pType = ProposalType.PULL;
 				Collections.sort(partnerAppList, new AppCPUComparator());
-				propAppList = getAppListToPropose((partnerCPUUsage - ownCPUUsage) / 2, partnerAppList, new ArrayList<ApplicationInfo>());
+				propAppList = getAppListToPropose((partnerCPUUsage - ownCPUUsage) / 2, partnerAppList, partnerReceivedApps, new HashSet<Integer>());
 			}
 		}
 		
 		return new Proposal(pType, propAppList);
+	}
+	
+	@Override
+	public synchronized Proposal processProposal(final Proposal receivedProposal, final ApplicationsList ownAppList, final Set<Integer> ownPromisedApps) {
+		Proposal acceptedProposal = null;
+		switch (receivedProposal.getProposalType()) {
+			case PUSH:
+				// Receive Apps - Check that there is still available CPU Usage
+				if (ownAppList.totalCPUDemand() >= receivedProposal.getApplicationsList().totalCPUDemand()) {
+					acceptedProposal = receivedProposal;
+				} else {
+					ApplicationsList newAppSet = buildAppSetFromProposal(receivedProposal, new ApplicationsList());
+					acceptedProposal = new Proposal(receivedProposal.getProposalType(), newAppSet);
+				}
+				break;
+			case PULL:
+				// Send Apps - Check that the Apps requested are still available
+				ApplicationsList newAppSet = buildAppSetFromProposal(receivedProposal, ownPromisedApps);
+				acceptedProposal = new Proposal(receivedProposal.getProposalType(), newAppSet);
+				break;
+			default:
+				// Unknown proposal type - Create a fake reply
+				acceptedProposal = new Proposal(receivedProposal.getProposalType(), new ApplicationsList());
+				break;
+		}
+		
+		if (acceptedProposal.getApplicationsList().isEmpty()) {
+			acceptedProposal = new Proposal(ProposalType.NO_ACTION, null);
+		}
+		
+		return acceptedProposal;
+	}
+	
+	private ApplicationsList buildAppSetFromProposal(final Proposal receivedProposal, final ApplicationsList leasedAppList) {
+		ApplicationsList appList = new ApplicationsList();
+		double usedCPUUnits = 0;
+		for (Application app : receivedProposal.getApplicationsList()) {
+			if (!appInfo.appMoved() && !leasedAppList.contains(appInfo)) {
+				double appCPUDemand = appInfo.getApplication().getCPUDemand();
+				if (usedCPUUnits + appCPUDemand <= getTotalDemand()) {
+					appInfoList.add(appInfo);
+					usedCPUUnits += appCPUDemand;
+				}
+			}
+		}
+		return appInfoList;
 	}
 }
--- EP2400-Distributed_Resource_Allocation/src/peersim/EP2400/resourcealloc/tasks/placementStartegy/PlacementStrategy.java
+++ EP2400-Distributed_Resource_Allocation/src/peersim/EP2400/resourcealloc/tasks/placementStartegy/PlacementStrategy.java
@@ -1,28 +1,18 @@
 package peersim.EP2400.resourcealloc.tasks.placementStartegy;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Set;
 
 import peersim.EP2400.resourcealloc.base.Application;
-import peersim.EP2400.resourcealloc.tasks.util.ApplicationInfo;
+import peersim.EP2400.resourcealloc.base.ApplicationsList;
 import peersim.EP2400.resourcealloc.tasks.util.Proposal;
 
 public abstract class PlacementStrategy {
-	public abstract Proposal getProposal(final List<ApplicationInfo> ownAppList, final List<ApplicationInfo> partnerAppList,
-		final List<ApplicationInfo> leasedAppList);
+	public abstract Proposal getProposal(final ApplicationsList ownAppList, final ApplicationsList partnerAppList, final Set<Integer> ownReceivedApps,
+		final Set<Integer> partnerReceivedApps, final Set<Integer> ownPromisedApps);
 	
-	/**
-	 * This method calculate the amount of CPU used by a set of Apps
-	 * 
-	 * @param appList
-	 * @return
-	 */
-	public double getCPUUsage(final List<ApplicationInfo> appList) {
-		double cpuUsage = 0;
-		for (ApplicationInfo appInfo : appList) {
-			cpuUsage += appInfo.getApplication().getCPUDemand();
-		}
-		return cpuUsage;
+	public synchronized Proposal processProposal(final Proposal receivedProposal, final ApplicationsList ownAppList, final Set<Integer> ownPromisedApps) {
+		// Override when required
+		return receivedProposal;
 	}
 	
 	/**
@@ -36,24 +26,20 @@ public abstract class PlacementStrategy {
 	*            not propose it to someone else
 	* @return
 	*/
-	protected List<ApplicationInfo> getAppListToPropose(final double cpuUnits, final List<ApplicationInfo> appList, List<ApplicationInfo> leasedAppList) {
-		return getAppListToPropose(cpuUnits, appList, leasedAppList, false);
+	protected ApplicationsList getAppListToPropose(final double cpuUnits, final ApplicationsList appList, final Set<Integer> promisedApps,
+		final Set<Integer> receivedApps) {
+		return getAppListToPropose(cpuUnits, appList, promisedApps, receivedApps, false);
 	}
 	
-	protected List<ApplicationInfo> getAppListToPropose(final double cpuUnits, final List<ApplicationInfo> appList, List<ApplicationInfo> leasedAppList,
-		final boolean returnAtLeastSmallestApp) {
-		List<ApplicationInfo> retList = new ArrayList<ApplicationInfo>();
+	protected ApplicationsList getAppListToPropose(final double cpuUnits, final ApplicationsList appList, final Set<Integer> promisedApps,
+		final Set<Integer> receivedApps, final boolean returnAtLeastSmallestApp) {
 		double usedCPUUnits = 0;
-		ApplicationInfo smallestApp = new ApplicationInfo(new Application(0, Integer.MAX_VALUE, Integer.MAX_VALUE), null);
+		ApplicationsList retList = new ApplicationsList();
+		ApplicationsList smallestApp = new ApplicationsList();
 		
-		// first i try to move applications that someone else gave to me because
-		// i already paid the virtual cost of moving the app
-		// the cost for moving the app in one epoch is 1 if you move it once or
-		// move it 30 times
-		// because the actual phisical moving is done only at the end of the
-		// epoch
-		for (ApplicationInfo appInfo : appList) {
-			if (appInfo.appMoved() && !leasedAppList.contains(appInfo)) {
+		// Try to move applications that someone else gave to the node in order to minimize the reconfiguration cost
+		for (Application appInfo : appList) {
+			if (!leasedAppList.contains(appInfo)) {
 				double appCPUDemand = appInfo.getApplication().getCPUDemand();
 				if (usedCPUUnits + appCPUDemand <= cpuUnits) {
 					retList.add(appInfo);
