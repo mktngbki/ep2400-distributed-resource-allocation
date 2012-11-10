76198af9ff07fee9ff3bf87b4a55992508669cfe
ormenisan.adrian@gmail.com <ormenisan.adrian@gmail.com@960aee3d-9021-a212-2a8e-b595aefb4307>
2012-11-10 18:16:34 +0000

    Final EnergyEfficiency for testing

--- EP2400-Distributed_Resource_Allocation/src/peersim/EP2400/resourcealloc/tasks/simpleStrategy/EnergyEfficiencyStrategy.java
+++ EP2400-Distributed_Resource_Allocation/src/peersim/EP2400/resourcealloc/tasks/simpleStrategy/EnergyEfficiencyStrategy.java
@@ -8,104 +8,174 @@ import java.util.Set;
 import peersim.EP2400.resourcealloc.base.Application;
 import peersim.EP2400.resourcealloc.base.ApplicationsList;
 import peersim.EP2400.resourcealloc.tasks.util.AppCPUComparator;
-import peersim.EP2400.resourcealloc.tasks.util.AppListInfo;
+import peersim.EP2400.resourcealloc.tasks.util.NodeView;
 
 public class EnergyEfficiencyStrategy extends Strategy {
 
 	private double cpuCapacity;
-	
+
 	public EnergyEfficiencyStrategy(double cpuCapacity) {
 		this.cpuCapacity = cpuCapacity;
 	}
-	
-	@Override
-	public Result getPlacement(AppListInfo activeList, AppListInfo passiveList) {
-		
-		Set<Integer> activeMovedAppIds = activeList.getMovedApps();
-		Set<Integer> passiveMovedAppIds = passiveList.getMovedApps();
 
-		Auxiliary activeSplitResult = splitNativeReceived(activeList.getAppList(), activeMovedAppIds);
-		Auxiliary passiveSplitResult = splitNativeReceived(passiveList.getAppList(), passiveMovedAppIds);
+	@Override
+	public Result getPlacement(NodeView activeView, NodeView passiveView) {
+		final ApplicationsList initActiveApps = activeView.getAppList();
+		final ApplicationsList initPassiveApps = passiveView.getAppList();
+		final Set<Integer> activeMovedAppIds = activeView.getMovedApps();
+		final Set<Integer> passiveMovedAppIds = passiveView.getMovedApps();
+		final Auxiliary activeSplitResult = splitNativeReceived(initActiveApps, activeMovedAppIds);
+		final Auxiliary passiveSplitResult = splitNativeReceived(initPassiveApps, passiveMovedAppIds);
+		final double activeNativeCPU = activeSplitResult.getListNative().totalCPUDemand();
+		final double passiveNativeCPU = passiveSplitResult.getListNative().totalCPUDemand();
+		final double activeMovedCPU = activeSplitResult.getListMoved().totalCPUDemand();
+		final double passiveMovedCPU = passiveSplitResult.getListMoved().totalCPUDemand();
+		final double initActiveCPU = activeNativeCPU + activeMovedCPU;
+		final double initPassiveCPU = passiveNativeCPU + passiveMovedCPU;
+
+		Result result = new Result();
+		//if one of the servers is inactive, we cannot do a better energy efficiency placement
+		if(0 == initActiveCPU || 0 == initPassiveCPU) {
+			result.setActiveMovedAppIds(activeMovedAppIds);
+			result.setPassiveMovedAppIds(passiveMovedAppIds);
+			return result;
+		}
+
+		//if we can move all the applications to one server, this is the best energy efficiency placement
+		if(initActiveCPU + initPassiveCPU <= cpuCapacity) {
+			result.setActiveAllocated(new HashSet<Application>(initPassiveApps));
+			for (Application app : initPassiveApps) {
+				activeMovedAppIds.add(app.getID());
+			}
+			result.setActiveMovedAppIds(activeMovedAppIds);
+			result.setPassiveDeallocated(new HashSet<Application>(initPassiveApps));
+			return result;
+		}
+
+		//calculate initial variance
+		//we will decide if the new placement is better based on variance comparison
+		double initAvg = (initActiveCPU + initPassiveCPU)/2;
+		double initStd = Math.sqrt((Math.pow(initActiveCPU - initAvg, 2) + Math.pow(initPassiveCPU - initAvg, 2))/2);
+		double initVar = initStd/initAvg;
+
+		if(initActiveApps.totalCPUDemand() == initPassiveApps.totalCPUDemand()) {
+			result = new Result();
+			result.setActiveMovedAppIds(activeMovedAppIds);
+			result.setPassiveMovedAppIds(passiveMovedAppIds);
+			return result;
+		}
 
 		ApplicationsList activeNativeApps = activeSplitResult.getListNative();
-		ApplicationsList pasiveNativeApps = passiveSplitResult.getListNative();
+		ApplicationsList passiveNativeApps = passiveSplitResult.getListNative();
 		ApplicationsList movedApps = new ApplicationsList();
 		movedApps.addAll(activeSplitResult.getListMoved());
 		movedApps.addAll(passiveSplitResult.getListMoved());
 
-		double activeNativeCPU = activeNativeApps.totalCPUDemand();
-		double passiveNativeCPU = pasiveNativeApps.totalCPUDemand();
+		Set<Integer> lowerMovedAppIds = new HashSet<Integer>();
+		Set<Integer> higherMovedAppIds = new HashSet<Integer>();
+		Set<Application> higherAllocated = new HashSet<Application>();
+		Set<Application> higherDeallocated = new HashSet<Application>();
+		Set<Application> lowerAllocated = new HashSet<Application>();
+		Set<Application> lowerDeallocated = new HashSet<Application>();
+		ApplicationsList lowerApps = new ApplicationsList();
 
-		//this list we try to get it as close as we can to cpuCapacity, without going over tau
-		ApplicationsList maxApps = new ApplicationsList();
-		Set<Integer> maxMovedApps = new HashSet<Integer>();
-		double maxCPU;
-		//this list we try to get it as close as we can to 0 so that we can shut down the server
-		ApplicationsList minApps = new ApplicationsList();
-		Set<Integer> minMovedApps = new HashSet<Integer>();
-
-		if(activeNativeCPU > tau) {
-			//move max and then see
-		} else if(passiveNativeCPU > tau) {
-
-		}
+		double higherCPU;
 		if(activeNativeCPU > passiveNativeCPU) {
-			maxApps.addAll(activeNativeApps);
-			maxCPU = activeNativeCPU;
-			minApps.addAll(pasiveNativeApps);
+			lowerApps.addAll(passiveNativeApps);
+			higherCPU = activeNativeCPU;
+			lowerMovedAppIds = passiveMovedAppIds; 
+			higherMovedAppIds = activeMovedAppIds;
 		} else {
-			maxApps.addAll(pasiveNativeApps);
-			maxCPU = passiveNativeCPU;
-			minApps.addAll(activeNativeApps);
+			lowerApps.addAll(activeNativeApps);
+			higherCPU = passiveNativeCPU;
+			lowerMovedAppIds = activeMovedAppIds;
+			higherMovedAppIds = passiveMovedAppIds;
 		}
 
-		//first we try to move as many of the moved apps to the maximized node
 		Collections.sort(movedApps, new AppCPUComparator());
 		Iterator<Application> it = movedApps.iterator();
 		while(it.hasNext()) {
-			Application app = it.next();
-			if(maxCPU + app.getCPUDemand() <= tau) {
-				maxCPU = maxCPU + app.getCPUDemand();
-				maxApps.add(app);
-				maxMovedApps.add(app.getID());
-				it.remove();
+			if(cpuCapacity == higherCPU) {
+				break;
 			}
-		}
-
-		//now we try to move any native apps to the maximized node if possible
-		if(maxCPU < tau) {
-			it = minApps.iterator();
-			while(it.hasNext()) {
-				Application app = it.next();
-				if(maxCPU + app.getCPUDemand() <= tau) {
-					maxCPU = maxCPU + app.getCPUDemand();
-					maxApps.add(app);
-					maxMovedApps.add(app.getID());
-					it.remove();
+			Application app = it.next();
+			if(cpuCapacity - higherCPU >= app.getCPUDemand()) {
+				if(lowerMovedAppIds.contains(app.getID())) {
+					higherAllocated.add(app);
+					lowerDeallocated.add(app);
+					lowerMovedAppIds.remove(app.getID());
+					higherMovedAppIds.add(app.getID());
 				}
+				it.remove();
+				higherCPU = higherCPU - app.getCPUDemand();
 			}
 		}
 
-		//add all of the moved apps that cannot fit to the maximized node to the minimized node
-		it = movedApps.iterator();
+		//fill in on the higher with native applications from the lower
+		Collections.sort(lowerApps, new AppCPUComparator());
+		it = lowerApps.iterator();
 		while(it.hasNext()) {
+			if(cpuCapacity == higherCPU) {
+				break;
+			}
 			Application app = it.next();
-			minApps.add(app);
-			minMovedApps.add(app.getID());
-			it.remove();
+			if(cpuCapacity - higherCPU >= app.getCPUDemand()) {
+				higherAllocated.add(app);
+				higherMovedAppIds.add(app.getID());
+				lowerDeallocated.add(app);
+				it.remove();
+				higherCPU = higherCPU - app.getCPUDemand();
+			}
 		}
 
-		AppListInfo list1Result;
-		AppListInfo list2Result;
+		//add all unallocated moved apps to the lower node
+		for (Application app : movedApps) {
+			if(!lowerMovedAppIds.contains(app.getID())) {
+				higherDeallocated.add(app);
+				higherMovedAppIds.remove(app.getID());
+				lowerAllocated.add(app);
+				lowerMovedAppIds.add(app.getID());
+			}
+		}
+
 		if(activeNativeCPU > passiveNativeCPU) {
-			list1Result = new AppListInfo(maxApps, maxMovedApps);
-			list2Result = new AppListInfo(minApps, minMovedApps);
+			result.setActiveAllocated(higherAllocated);
+			result.setActiveDeallocated(higherDeallocated);
+			result.setActiveMovedAppIds(higherMovedAppIds);
+			result.setPassiveAllocated(lowerAllocated);
+			result.setPassiveDeallocated(lowerDeallocated);
+			result.setPassiveMovedAppIds(lowerMovedAppIds);
 		} else {
-			list1Result = new AppListInfo(minApps, minMovedApps);
-			list2Result = new AppListInfo(maxApps, maxMovedApps);
+			result.setActiveAllocated(lowerAllocated);
+			result.setActiveDeallocated(lowerDeallocated);
+			result.setActiveMovedAppIds(lowerMovedAppIds);
+			result.setPassiveAllocated(higherAllocated);
+			result.setPassiveDeallocated(higherDeallocated);
+			result.setPassiveMovedAppIds(higherMovedAppIds);
 		}
 
-		return new Result(list1Result, list2Result);
+		ApplicationsList finalActiveApps = new ApplicationsList();
+		finalActiveApps.addAll(initActiveApps);
+		finalActiveApps.addAll(result.getActiveAllocated());
+		finalActiveApps.removeAll(result.getActiveDeallocated());
+		
+		ApplicationsList finalPassiveApps = new ApplicationsList();
+		finalPassiveApps.addAll(initPassiveApps);
+		finalPassiveApps.addAll(result.getPassiveAllocated());
+		finalPassiveApps.removeAll(result.getPassiveDeallocated());
+		
+		double finalActiveCPU = finalActiveApps.totalCPUDemand();
+		double finalPassiveCPU = finalPassiveApps.totalCPUDemand();
+		double finalAvg = (finalActiveCPU + finalPassiveCPU)/2;
+		double finalStd = Math.sqrt((Math.pow(finalActiveCPU - finalAvg, 2) + Math.pow(finalPassiveCPU - finalAvg, 2))/2);
+		double finalVar = finalStd/finalAvg;
+		
+		//if our result is worse that initial placement we keep initial placement
+		if(finalVar < initVar) {
+			result.setActiveMovedAppIds(activeMovedAppIds);
+			result.setPassiveMovedAppIds(passiveMovedAppIds);
+		}
+		return result;
 	}
 
 }
