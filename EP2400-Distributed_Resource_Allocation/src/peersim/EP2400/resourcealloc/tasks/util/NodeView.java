62513e7947cbdc92e200ec3b94e84afed1b86f85
sercuc@gmail.com <sercuc@gmail.com@960aee3d-9021-a212-2a8e-b595aefb4307>
2012-11-10 15:34:26 +0000

    AppListInfo renamed to NodeView

---
+++ EP2400-Distributed_Resource_Allocation/src/peersim/EP2400/resourcealloc/tasks/util/NodeView.java
@@ -0,0 +1,35 @@
+package peersim.EP2400.resourcealloc.tasks.util;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import peersim.EP2400.resourcealloc.base.ApplicationsList;
+
+public class NodeView {
+	private ApplicationsList	appList;
+	private Set<Integer>		receivedApps;
+	private double				currentSystemLoadView;
+	
+	public NodeView(ApplicationsList appList, double currentSystemLoadView) {
+		this.appList = appList;
+		receivedApps = new HashSet<Integer>();
+		this.currentSystemLoadView = currentSystemLoadView;
+	}
+	
+	public NodeView(ApplicationsList appList, Set<Integer> receivedApps) {
+		this.appList = appList;
+		this.receivedApps = receivedApps;
+	}
+	
+	public ApplicationsList getAppList() {
+		return appList;
+	}
+	
+	public Set<Integer> getMovedApps() {
+		return receivedApps;
+	}
+	
+	public double getCurrentSystemLoadView() {
+		return currentSystemLoadView;
+	}
+}
